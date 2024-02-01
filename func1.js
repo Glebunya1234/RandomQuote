5a3d8b3619555d979b1fc4a3c9da19a21c05b4be
Glebunya1234 <glebbuckiy@gmail.com>
2024-02-01 20:11:23 +0200

    RandQuote

---
+++ func1.js
@@ -0,0 +1,26 @@
+const getRandomQuote = () => {
+  $.ajax({
+    url: "https://api.quotable.io/random",
+    method: "GET",
+    success: function (response) {
+      displayQuote(response);
+    },
+    error: function (error) {
+      console.error("Error fetching quote:", error);
+    },
+  });
+};
+
+$("#new-quote-btn").click(function () {
+  getRandomQuote();
+});
+
+$(document).ready(function () {
+  getRandomQuote();
+});
+
+const displayQuote = (quote) => {
+  $("#quote-text").text(quote.content);
+  $("#quote-author").text(quote.author);
+};
+
